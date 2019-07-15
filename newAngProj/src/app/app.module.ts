d6b6cde6893f5f8eb47d09254e7a2e55206a2471
sergeypakylin <sergey.pakylin@saritasa.com>
2019-07-15 18:34:20 +0500

    "Preparations to login window"

--- newAngProj/src/app/app.module.ts
+++ newAngProj/src/app/app.module.ts
@@ -3,13 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { ClientModule } from './client/client.module';
+import { LoginComponent } from './shared/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    CoreModule,
+    SharedModule,
+    ClientModule,
     AppRoutingModule
   ],
   providers: [],
