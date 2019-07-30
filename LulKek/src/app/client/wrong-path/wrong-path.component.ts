ab04d383bd15a42f8f9fccb026c2d5ee19a2e8a5
sergeypakylin <sergey.pakylin@saritasa.com>
2019-07-30 15:55:19 +0500

    changed components name

---
+++ LulKek/src/app/client/wrong-path/wrong-path.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+/**
+ * Page not-found component.
+ */
+@Component({
+  selector: 'app-wrong-path',
+  templateUrl: './wrong-path.component.html',
+  styleUrls: ['./wrong-path.component.css'],
+})
+export class WrongPathComponent {
+
+}
