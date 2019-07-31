c671affae0a50f3aa6d634ac170b6c1c013b63a6
sergeypakylin <sergey.pakylin@saritasa.com>
2019-07-31 11:43:22 +0500

    Done with character task

---
+++ LulKek/src/app/core/services/dtos/user-dto.ts
@@ -0,0 +1,15 @@
+***REMOVED****
+***REMOVED*** Dto in terms of http post-request.
+***REMOVED***/
+export interface UserDto {
+
+***REMOVED*****REMOVED*****REMOVED***
+   ***REMOVED*****REMOVED*** user idToken.
+***REMOVED*****REMOVED*****REMOVED***
+    idToken: string;
+
+***REMOVED*****REMOVED*****REMOVED***
+   ***REMOVED*****REMOVED*** user email.
+***REMOVED*****REMOVED*****REMOVED***
+    email: string;
+}
