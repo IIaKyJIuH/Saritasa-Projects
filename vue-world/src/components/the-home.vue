cf4742ec6c752895c836871908ffb62ba11a34a5
sergeypakylin <sergey.pakylin@saritasa.com>
2019-08-06 13:51:23 +0500

    Style fixes + film comp.

---
+++ vue-world/src/components/the-home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TheHome',
+    data () {
+      return {
+        msg: 'Welcome to my first Vue.js app'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+</style>
