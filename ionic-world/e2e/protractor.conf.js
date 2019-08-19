b44d290ed9608850011f805a2ef0685b0a127cf5
sergeypakylin <sergey.pakylin@saritasa.com>
2019-08-19 15:07:37 +0500

    Initialized ionic

---
+++ ionic-world/e2e/protractor.conf.js
@@ -0,0 +1,28 @@
+// Protractor configuration file, see link for more information
+// https://github.com/angular/protractor/blob/master/lib/config.ts
+
+const { SpecReporter } = require('jasmine-spec-reporter');
+
+exports.config = {
+  allScriptsTimeout: 11000,
+  specs: [
+    './src***REMOVED*******REMOVED***.e2e-spec.ts'
+  ],
+  capabilities: {
+    'browserName': 'chrome'
+***REMOVED***
+  directConnect: true,
+  baseUrl: 'http://localhost:4200/',
+  framework: 'jasmine',
+  jasmineNodeOpts: {
+    showColors: true,
+    defaultTimeoutInterval: 30000,
+    print: function() {}
+***REMOVED***
+  onPrepare() {
+    require('ts-node').register({
+      project: require('path').join(__dirname, './tsconfig.json')
+    });
+    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+  }
+***REMOVED***
---
+++ ionic-world/src/zone-flags.ts
@@ -0,0 +1,5 @@
+***REMOVED****
+***REMOVED*** Prevents Angular change detection from
+***REMOVED*** running with certain Web Component callbacks
+***REMOVED***/
+(window as any).__Zone_disable_customElements = true;
