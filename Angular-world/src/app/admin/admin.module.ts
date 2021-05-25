48b067cb8a8fac9264c075f8a6f3cbb439691e87
Sergei Pakulin <pakylin3@mail.ru>
2021-05-25 17:47:03 +0500

    upgraded to angular9 material

--- Angular-world/src/app/admin/admin.module.ts
+++ Angular-world/src/app/admin/admin.module.ts
@@ -1,12 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {
-  MatCardModule,
-  MatFormFieldModule,
-  MatInputModule,
-  MatButtonModule,
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
 
